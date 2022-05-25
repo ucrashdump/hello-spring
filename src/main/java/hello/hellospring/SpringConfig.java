3a54667cbb280f492297555af4c6b15289d30109
ucrashdump <ucrashdump@gmail.com>
2022-05-25 23:53:20 +0900

    onGoing

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    // 상황에 따라 구현클래스를 변경해야할 경우
+    // 운영 코드 일절 건들필요 없게 만드는 경우.
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,12 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(){
+        this.name = name;
+    }
+}
